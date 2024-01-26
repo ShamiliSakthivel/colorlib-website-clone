773bbb4ed8ecc88904c8250948ecf62e05219b6b
user <user@LAPTOP-KQ0FH290>
2024-01-26 18:09:31 +0400

    Add project files.

---
+++ Colorlib.com/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Colorlib.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        public IActionResult Aboutus()
+        {
+            return View(); 
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+        public IActionResult Work()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+    }
+}
